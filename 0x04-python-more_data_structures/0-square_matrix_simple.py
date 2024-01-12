8755ce3bf75e4c96389349bc66daab187a60925a
Symomkuu <simonnjunge350@gmail.com>
2024-01-12 08:42:28 -0800

    matrrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(new_matrix):
-            new_matrix[idx][idx2] = row[idx2] ** 2
-    return new_matrix 
+    new = [[matrix[i][j] ** 2 for i in range(len(matrix))]
+           for j in range(len(matrix[0]))]
+    return new
