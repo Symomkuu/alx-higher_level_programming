68db1c552c773cbee84571da2717829b495c2047
Symomkuu <simonnjunge350@gmail.com>
2024-01-22 02:08:50 -0800

    index out of range

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        y = 0
+        for i in range(x):
+            print(my_list[i], end = "")
+            y = y + 1
+        print()
+    except IndexError:
+        print("")
+    return (y)
